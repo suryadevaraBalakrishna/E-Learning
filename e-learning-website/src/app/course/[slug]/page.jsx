33694274e12b6d3d62ae3afcc5ea10d54091099b
Suryadevara Balakrishna <balakrishnasuryadevara@gmail.com>
2026-03-29 17:44:29 +0530

    fixed search params issue

--- e-learning-website/src/app/course/[slug]/page.jsx
+++ e-learning-website/src/app/course/[slug]/page.jsx
@@ -1,5 +1,5 @@
-export const dynamic = "force-dynamic";
 "use client";
+export const dynamic = "force-dynamic";
 import { useParams } from "next/navigation";
 
 import CourseItem from '@/app/components/common/CourseItem'
